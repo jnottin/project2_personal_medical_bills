4fca493008969771f5f4cac9f698f87c2a19af47
Sam Nottingham <jnottin92@gmail.com>
2018-11-15 14:47:30 -0500

    trying to deploy

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost/movieTrailers", { useNewUrlParser: true });
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+} else {
+    mongoose.connect("mongodb://localhost/movieTrailers");
+}
 mongoose.Promise = Promise;
 module.exports = mongoose;
+
